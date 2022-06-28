df494122b964cdff98306085120fb15807f98299
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-28 17:51:19 -0500

    test

---
+++ Develop/script.js
@@ -0,0 +1,3 @@
+// <!-- If you have been using hard-coded <li> items in your HTML to expedite testing the app up to now, you must delete those items before continuing. The JavaScript that follows will not work as described if there are any hard-coded <li> items in the task lists in your HTML.
+
+//   $("#modalDueDate").datepicker(); --> from mod 5.4.4
