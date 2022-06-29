7c97e03e6bedc25af48e1006eda4e8990bae8d5a
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-29 12:55:13 -0500

    added time and date on screen

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,36 @@
+// variables to get time and date on screen 
+var weekday = document.getElementById('currentDay')
+var timeNow = document.getElementById('currentTime')
 
+var d = new Date();
+var dayofweek = d.getDay();
+var date= d.toLocaleString();
 
-
-$("container").css("margin","500x");
-
+switch(dayofweek){
+    case 0:
+        dayofweek = "Sunday";
+        break;
+    case 1:
+        dayofweek = "Monday";
+        break; 
+    case 2:
+        dayofweek = "Tuesday";
+        break;
+    case 3:
+        dayofweek = "Wednesday";
+        break;
+    case 4:
+        dayofweek = "Thursday";
+        break;
+    case 5:
+        dayofweek = "Friday";
+        break;
+    case 6: 
+        dayofweek = "Saturday"   
+}  
+     
+weekday.innerHTML = dayofweek;
+currentTime.innerHTML = date;
 // save events 
 // var saveEvents = function() {
 //     localStorage.setItem("events", JSON.stringify(tasks));
