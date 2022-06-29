bbcac3f9dd424023829481ba301f3a639d626e31
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-29 16:22:32 -0500

    local storage issues

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var timeNow = document.getElementById('currentTime')
 
 var d = new Date();
 var dayofweek = d.getDay();
-var hourOfTimEoFdAYoFToday = d.getHours()
-console.log(hourOfTimEoFdAYoFToday)
+var hoursRealTime = d.getHours()
+console.log(hoursRealTime)
 var date= d.toLocaleString();
 
-var hours = ["9AM", "10AM", "11AM"]
+var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM" ]
 
 switch(dayofweek){
     case 0:
@@ -32,15 +32,25 @@ switch(dayofweek){
     case 6: 
         dayofweek = "Saturday"   
 }  
-     
+
+// $(document).ready(function() {
+//     for (var i=0; i < hours.lenth; i++)
+//     $('<div class="row" </div>').appendTo('.container');
+// });
+
+// $('<div class="row" </div>').appendTo('.container');
+
 weekday.innerHTML = dayofweek;
 currentTime.innerHTML = date;
+
+// local storage code 
+var obj = "hi"
 
 
 localStorage.setItem("whatever II: Electric Boogaloo", JSON.stringify(obj))
 
-var IgotSomethingFromLocal = JSON.parse(localStorage.getItem("whatever"))
-console.log(IgotSomethingFromLocal)
+// var IgotSomethingFromLocal = JSON.parse(localStorage.getItem("whatever"))
+// console.log(IgotSomethingFromLocal)
 
 // save events 
 // var saveEvents = function() {
