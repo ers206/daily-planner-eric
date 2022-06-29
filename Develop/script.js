ee530b6cd5ab7b714515dcfa06378621cd56c958
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-28 20:27:35 -0500

    just starting

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
-// <!-- If you have been using hard-coded <li> items in your HTML to expedite testing the app up to now, you must delete those items before continuing. The JavaScript that follows will not work as described if there are any hard-coded <li> items in the task lists in your HTML.
 
-//   $("#modalDueDate").datepicker(); --> from mod 5.4.4
+
+
+$("container").css("margin","500x");
+
+// save events 
+// var saveEvents = function() {
+//     localStorage.setItem("events", JSON.stringify(tasks));
+//   };
+  
